6991e9140908485177505a1b3ea0db78b8e1371d
Timur <serjasum1994@icloud.com>
2023-06-27 00:25:11 +0500

    Добавлена генерация счета для доступа к маркету.

--- cabinet/views.py
+++ cabinet/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import UserProfile, Subscription
 from django.contrib.auth.decorators import login_required
 from .forms import SettingsProfileForm
+from .payment import create_bill, check_state
 
 
 def profile(request, pk):
@@ -20,7 +21,14 @@ def profile(request, pk):
 
 @login_required
 def settings_profile(request):
+    bill = create_bill(100)
     form = SettingsProfileForm(request.POST, request.FILES)
+    if check_state(bill['id']):
+        user = get_object_or_404(UserProfile, pk=request.user.pk)
+        print(user)
+    else:
+        print('no')
+
     if request.method == 'POST':
         if form.is_valid():
             cleaned_data = form.cleaned_data
@@ -28,4 +36,4 @@ def settings_profile(request):
                 if field_value:
                     setattr(request.user, field_name, field_value)
             UserProfile.save(request.user)
-    return render(request, 'cabinet/settings.html', {'form': form})
+    return render(request, 'cabinet/settings.html', {'form': form, 'url_payment': bill['url']})
