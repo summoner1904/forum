e609c23270a09c40c84f305b9355cbbbbb917921
Timur <serjasum1994@icloud.com>
2023-07-22 02:42:23 +0500

    Добавлен скрипт для загрузки фото с помощью Ctrl + V.

--- threads/forms.py
+++ threads/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class NewThreadForm(forms.Form):
     title = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'style': 'border: none; background: #2f2f2f;height: 40px; width: 60%;padding-left: 20px; color: white;'}))
     text = forms.CharField(max_length=10000, widget=forms.Textarea(attrs={'style': 'background: #2f2f2f;border:none;padding: 10px 0 0 20px;color: white;width: 60%;height: 300px;'}))
-    screen = forms.FileField(widget=forms.FileInput(), required=False)
+    screen = forms.ImageField(widget=forms.FileInput(attrs={'id': 'id_screen'}), required=False)
--- threads/urls.py
+++ threads/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_thread
+from .views import create_thread, thread
 
 app_name = 'threads'
 
 
 urlpatterns = [
     path('create_thread/', create_thread, name='new_thread'),
+    path('thread/<int:thread_id>', thread, name='thread')
 ]
