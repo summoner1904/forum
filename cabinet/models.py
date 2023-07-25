9409d561e783982d29da2ee7ea86f74ea72b0e41
Timur <serjasum1994@icloud.com>
2023-07-26 00:19:55 +0500

    Проведен рефакторинг кода.

--- cabinet/models.py
+++ cabinet/models.py
@@ -5,19 +5,45 @@ import datetime
 
 
 class UserProfile(AbstractUser):
-    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    # Стандартное изображение профиля нового пользователя
+    DEFAULT = 'images_profile/2023-05-27/caf098e477ae55905890b7fb0b3700c1.jpg'
+
+    date_joined = models.DateTimeField(verbose_name="date joined",
+                                       auto_now_add=True)
     status = models.CharField(max_length=128, blank=True)
-    date_birthday = models.DateField(blank=True, null=True, default='1970-01-01', verbose_name='День рождения')
-    telegram = models.CharField(max_length=64, default='Неизвестно', null=True, verbose_name='Telegram', blank=True)
-    steam = models.CharField(max_length=64, default='Неизвестно', null=True, verbose_name='Steam', blank=True)
-    discord = models.CharField(max_length=64, default='Неизвестно', null=True, verbose_name='Discord', blank=True)
-    gender = models.CharField(max_length=16, default='Не указан', null=True, verbose_name='Пол')
-    reputation = models.IntegerField(verbose_name='Репутация', default=0)
-    messages = models.IntegerField(verbose_name='Сообщения', default=0)
-    subscribes = models.IntegerField(verbose_name='Подписчики', default=0)
-    product = models.IntegerField(verbose_name='Товаров', default=0)
-    avatar = models.ImageField(upload_to='images_profile/%Y-%m-%d', default='images_profile/2023-05-27/caf098e477ae55905890b7fb0b3700c1.jpg')
-    access = models.BooleanField(verbose_name='Доступ к маркету', default=False)
+    date_birthday = models.DateField(
+        blank=True, null=True, default="1970-01-01",
+        verbose_name="День рождения"
+    )
+    telegram = models.CharField(
+        max_length=64,
+        default="Неизвестно",
+        null=True,
+        verbose_name="Telegram",
+        blank=True,
+    )
+    steam = models.CharField(
+        max_length=64, default="Неизвестно", null=True,
+        verbose_name="Steam", blank=True
+    )
+    discord = models.CharField(
+        max_length=64,
+        default="Неизвестно",
+        null=True,
+        verbose_name="Discord",
+        blank=True,
+    )
+    gender = models.CharField(
+        max_length=16, default="Не указан", null=True, verbose_name="Пол"
+    )
+    reputation = models.IntegerField(verbose_name="Репутация", default=0)
+    messages = models.IntegerField(verbose_name="Сообщения", default=0)
+    subscribes = models.IntegerField(verbose_name="Подписчики", default=0)
+    product = models.IntegerField(verbose_name="Товаров", default=0)
+    avatar = models.ImageField(
+        upload_to="images_profile/%Y-%m-%d",default=DEFAULT,
+    )
+    access = models.BooleanField(verbose_name="Доступ к маркету", default=False)
 
     def save(self, *args, **kwargs):
         self.last_login = datetime.datetime.now()
@@ -27,32 +53,49 @@ class UserProfile(AbstractUser):
             self._password = None
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
 
 
 UserProfile = get_user_model()
 
 
 class Subscription(models.Model):
-    subscriber = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='subscriptions_made',
-                                   verbose_name='Подписчик')
-    subscribed_to = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='subscriptions_received',
-                                      verbose_name='Подписан на')
+    subscriber = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        related_name="subscriptions_made",
+        verbose_name="Подписчик",
+    )
+    subscribed_to = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        related_name="subscriptions_received",
+        verbose_name="Подписан на",
+    )
 
 
 class Posts(models.Model):
-    sender = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='sent_posts',
-                               verbose_name='Отправитель')
-    poster = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='received_posts',
-                               verbose_name='Получатель')
-    text_post = models.CharField(max_length=6144, verbose_name='Текст поста')
-    created_at = models.DateTimeField(verbose_name='Время создания', auto_now=True)
+    sender = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        related_name="sent_posts",
+        verbose_name="Отправитель",
+    )
+    poster = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        related_name="received_posts",
+        verbose_name="Получатель",
+    )
+    text_post = models.CharField(max_length=6144, verbose_name="Текст поста")
+    created_at = models.DateTimeField(verbose_name="Время создания",
+                                      auto_now=True)
 
 
 class Bill(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name='Пользователь')
-    bill_id = models.CharField(max_length=128, verbose_name='ID Платежа')
-    state = models.BooleanField(verbose_name='Статус', default=False)
-
-
+    user = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    bill_id = models.CharField(max_length=128, verbose_name="ID Платежа")
+    state = models.BooleanField(verbose_name="Статус", default=False)
